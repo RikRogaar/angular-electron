07e691b9d0892c4de8139eef587fddb40d11746e
Rik Rogaar <rik@qlic.nl>
2024-02-02 16:10:19 +0100

    init proj

--- index.js
+++ index.js
@@ -1,17 +1,15 @@
 const { app, BrowserWindow } = require('electron');
-const url = require('url');
-const path = require('path');
 
 function onReady () {
-	win = new BrowserWindow({width: 900, height: 6700})
-	// win.loadURL(url.format({
-	// 	pathname: path.join(
-	// 		__dirname,
-	// 		'dist/angular-electron/browser/index.html'),
-	// 	protocol: 'file:',
-	// 	slashes: true
-	// }))
+	win = new BrowserWindow({
+		width: 900,
+		height: 6700,
+	});
 	win.loadURL('http://localhost:4200');
 }
 
 app.on('ready', onReady);
+
+
+// vibrancy: 'fullscreen-ui',    // on MacOS
+// backgroundMaterial: 'acrylic' // on Windows 11
